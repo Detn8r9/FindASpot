2d65502a4b8943a9c451fbcd2f81187809817384
Detn8r9 <159511516+Detn8r9@users.noreply.github.com>
2024-02-09 00:29:08 -0500

    Added simple country answers to specific user input

---
+++ FindASpot.py
@@ -0,0 +1,8 @@
+import random
+num = random.randint(1,5)
+Places = ["Germany It Is", "France Sounds Fantastic", "Spain Would Be Splendid", "United States, OK", "Switzerland, Sweet", "UK Sounds Like Fun"]
+ans = input("Where would you like to go?:")
+if ans == "Home":
+    print(Places[0])
+elif ans == "You Pick":
+    print(Places[num])
