9c46a8b084afdf02d2f68cac7a7eeb6737e5a92b
Detn8r9 <159511516+Detn8r9@users.noreply.github.com>
2024-02-09 10:55:46 -0500

    Adding Price Input(WIP)

--- FindASpot.py
+++ FindASpot.py
@@ -1,8 +1,13 @@
+                   
 import random
 num = random.randint(1,5)
 Places = ["Germany It Is", "France Sounds Fantastic", "Spain Would Be Splendid", "United States, OK", "Switzerland, Sweet", "UK Sounds Like Fun"]
-ans = input("Where would you like to go?:")
+ans = input("Where would you like to go?:" + "\n")
+price = input("How much do you want to spend?:" + "\n")
+
 if ans == "Home":
     print(Places[0])
 elif ans == "You Pick":
     print(Places[num])
+
+
